eb345c9d1c16cdef243cc2bbdc864fb64a2202a7
Andrew Wang <emyarod@users.noreply.github.com>
2014-08-01 02:56:24 -0700

    greeting call and response

--- orcabot.js
+++ orcabot.js
@@ -1,8 +1,9 @@
-// create config
+// configure bot
 var config = {
 	channels: ["#house"],
 	server: "irc.snoonet.org",
-	botName: "orcabot"
+	botName: "orcabot",
+	realName: "https://github.com/emyarod/orcabot",
 };
 
 // get lib
@@ -10,10 +11,17 @@ var irc = require("irc");
 
 // bot name
 var bot = new irc.Client(config.server, config.botName, {
-	channels: config.channels
+	channels: config.channels,
+	realName: config.realName
 });
 
 // Listen for any message, say to him/her in the room
-bot.addListener("message", function(from, to, text, message) {
-	bot.say(config.channels[0], "(⊙ ◡ ⊙)");
+// bot.addListener("message", function(from, to, text, message) {
+// 	bot.say(config.channels[0], "(⊙ ◡ ⊙)");
+// });
+
+bot.addListener("message", function(from, to, message) {
+    if(message.indexOf('hi') > -1) {
+        bot.say(to, '(⊙ ◡ ⊙)');
+    }
 });
