d68e179088acb25ee3ab5eabdf7f38dbea3aacf1
Andrew Wang <emyarod@users.noreply.github.com>
2014-07-31 20:09:29 -0700

    config
    create config

---
+++ config.js
@@ -0,0 +1,19 @@
+// create config
+var config = {
+	channels: ["#house"],
+	server: "irc.snoonet.org",
+	botName: "orcabot"
+};
+
+// get lib
+var irc = require("irc");
+
+// bot name
+var bot = new irc.Client(config.server, config.botName, {
+	channels: config.channels
+});
+
+// Listen for any message, say to him/her in the room
+bot.addListener("message", function(from, to, text, message) {
+	bot.say(config.channels[0], "¿Public que?");
+});
