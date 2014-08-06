a8bce036b37fd0feec4bf69f3fcb64ebc49aa052
Andrew Wang <emyarod@users.noreply.github.com>
2014-08-06 15:12:26 -0700

    updated config

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var config = {
 	channels: ["#house"],
 	server: "irc.snoonet.org",
 	botName: "orcabot",
-	realName: "https://github.com/emyarod/orcabot",
+	realName: "https://github.com/emyarod/orcabot"
 };
 
 module.exports = config;
