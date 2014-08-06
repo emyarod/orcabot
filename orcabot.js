5a0a334d9e976c3b2205f2a519d46cf669187287
Andrew Wang <emyarod@users.noreply.github.com>
2014-08-06 15:12:36 -0700

    added twitter and google search module

--- orcabot.js
+++ orcabot.js
@@ -1,8 +1,11 @@
 // configure bot
 var config = require("./config");
 
-// get irc package
+// get node modules
 var irc = require("irc");
+var twit = require("twit");
+
+//get modules
 
 // bot name
 var bot = new irc.Client(config.server, config.botName, {
@@ -17,7 +20,35 @@ var bot = new irc.Client(config.server, config.botName, {
 // });
 
 bot.addListener("message", function(from, to, message) {
-    if(message.indexOf('hi') > -1) {
-        bot.say(to, '(⊙ ◡ ⊙)');
-    }
+    if(message.indexOf("hi") > -1) {
+    	bot.say(to, "(⊙ ◡ ⊙)");
+	}
+});
+
+// google
+
+bot.addListener("message", function(from, to, message) {
+	if(message.indexOf(".g") > -1) {
+		message = message.replace(".g ", "");
+		message = message.replace(/ /g, "+");
+		var searchLink = "https://google.com/search?q=" + message;
+		bot.say(to, searchLink);
+	}
+});
+
+// twitter
+
+var t = new twit({
+
+});
+
+
+bot.addListener("message", function(from, to, message) {
+	if(message.indexOf(".tw") > -1) {
+		message = message.replace(".tw ", "");
+		var screenName = message;
+		t.get("statuses/user_timeline", {screen_name: screenName, count: 1}, function (err, data, response) {
+			bot.say(to, "Most recent tweet by " + "\u0002" + data[0].user.name + "\u0002 "  + "(\u0002@" + screenName + "\u0002" + ")" + "\u0002 | \u0002" + data[0].text + "\u0002 | \u0002" + data[0].created_at);
+		});
+	}
 });
