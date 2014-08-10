bd6fb9a3f6ab7c826599d90bf8eedcba7bd9be80
Andrew Wang <emyarod@users.noreply.github.com>
2014-08-09 23:54:18 -0700

    added example config

---
+++ config.example.js
@@ -0,0 +1,14 @@
+var config = {
+	channels: [""],
+	server: "",
+	botName: "",
+	realName: "https://github.com/emyarod/orcabot",
+	twitterConsumerKey: "twitterConsumerKey",
+	twitterConsumerSecret: "twitterConsumerSecret",
+	twitterAccessToken: "twitterAccessToken",
+	twitterAccessTokenSecret: "twitterAccessTokenSecret",
+	lfmApiKey: "lfmApiKey",
+	lfmSecret: "lfmSecret"
+};
+
+module.exports = config;
