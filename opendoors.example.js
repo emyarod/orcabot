c799484b435f4e0ef61cb695d4722478f5d70230
Andrew Wang <emyarod@users.noreply.github.com>
2014-08-13 00:25:26 -0700

    example key storage

---
+++ opendoors.example.js
@@ -0,0 +1,12 @@
+var opendoors = {
+	twitterConsumerKey: "twitterConsumerKey",
+	twitterConsumerSecret: "twitterConsumerSecret",
+	twitterAccessToken: "twitterAccessToken",
+	twitterAccessTokenSecret: "twitterAccessTokenSecret",
+	lfmApiKey: "lfmApiKey",
+	lfmSecret: "lfmSecret",
+	igClientId: "igClientId",
+	igClientSecret: "igClientSecret",
+};
+
+module.exports = opendoors;
