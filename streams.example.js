38cdc696a5d69e4da70351830073c48b6515988d
emyarod <emyarod@users.noreply.github.com>
2014-10-13 03:23:22 -0400

    add example streams

---
+++ streams.example.js
@@ -0,0 +1,16 @@
+var streams = {
+  game: [
+	  {"user": "username0", "isLive": 0, "sentFlag": 0},
+	  {"user": "username1", "isLive": 0, "sentFlag": 0},
+	  {"user": "username2", "isLive": 0, "sentFlag": 0},
+	  {"user": "username3", "isLive": 0, "sentFlag": 0},
+	  {"user": "username4", "isLive": 0, "sentFlag": 0},
+	  {"user": "username5", "isLive": 0, "sentFlag": 0},
+	  {"user": "username6", "isLive": 0, "sentFlag": 0},
+	  {"user": "username7", "isLive": 0, "sentFlag": 0},
+	  {"user": "username8", "isLive": 0, "sentFlag": 0},
+	  {"user": "username9", "isLive": 0, "sentFlag": 0}
+  ],
+};
+
+module.exports = streams;
