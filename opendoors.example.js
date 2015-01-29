745884b08d326b3d58fbcb1249ea566a2b167966
emyarod <allllaboutyou@gmail.com>
2015-01-29 03:48:03 -0500

    update example keys

--- opendoors.example.js
+++ opendoors.example.js
@@ -9,6 +9,8 @@ var opendoors = {
 	igClientSecret: "igClientSecret",
 	googleCX: "INSERT YOUR CUSTOM SEARCH ENGINE ID here",
 	googleAPIKey: "INSERT YOUR API KEY HERE",
+	clientID: "client_ID",
+	msTranslatorKey: "client_secret",
 };
 
 module.exports = opendoors;
