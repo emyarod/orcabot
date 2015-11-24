5fea4804c7d5900805e8a43d13f8f5224aa7bc10
emyarod <allllaboutyou@gmail.com>
2015-11-24 18:15:36 -0500

    Add comments

--- opendoors.example.js
+++ opendoors.example.js
@@ -1,14 +1,19 @@
 var opendoors = {
+	// Twitter
 	twitterConsumerKey: "twitterConsumerKey",
 	twitterConsumerSecret: "twitterConsumerSecret",
 	twitterAccessToken: "twitterAccessToken",
 	twitterAccessTokenSecret: "twitterAccessTokenSecret",
+	// Last.fm
 	lfmApiKey: "lfmApiKey",
 	lfmSecret: "lfmSecret",
+	// Instagram
 	igClientId: "igClientId",
 	igClientSecret: "igClientSecret",
+	// Google APIs
 	googleCX: "INSERT YOUR CUSTOM SEARCH ENGINE ID here",
 	googleAPIKey: "INSERT YOUR API KEY HERE",
+	// Microsoft Translator
 	clientID: "client_ID",
 	msTranslatorKey: "client_secret",
 };
